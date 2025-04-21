7cf7351a63ce8bf43a50ef9cbbdda4b0c228cfaf
NightFuryGt1 <armanditosigo@gmail.com>
2025-04-21 11:57:58 -0600

    first commit

---
+++ 01-Fundamentos/js/primitivos.js
@@ -0,0 +1,17 @@
+//Tipos de datos primitivos String
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+//No se debe de reinicializar una variable con let
+ nombre= 'Ben Parker';
+console.log(nombre);
+
+//Es por mera preferencia usar tipos de comillas
+nombre = "Tia May";
+nombre = 'Tia May';
+
+//Como saber el tipo de dato de una variable
+console.log(typeof nombre);
+
+nombre= 123;
+console.log(typeof nombre);
